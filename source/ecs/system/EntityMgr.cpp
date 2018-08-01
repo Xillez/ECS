bf130a37418e1eea75e3c71af7c195b56c634f32
Xillez <kentwh@stud.ntnu.no>
2018-08-01 21:52:42 +0200

    Completed EntityMgr for now. Added a bunch new functions and other minor changes

--- source/ecs/system/EntityMgr.cpp
+++ source/ecs/system/EntityMgr.cpp
@@ -7,20 +7,36 @@ EntityMgr::EntityMgr()
 
 EntityID EntityMgr::createEntity()
 {
-    //
+    Entity* temp = new Entity();        // Make new Entity.
+    this->entityIDs.push_back(nextID);  // Add id.
+    this->entities[nextID] = temp;      // Save entity pointer.
+    temp = nullptr;
+    this->nextID++;                     // Ready for next entity registration.
 }
 
-Entity* getEntityByID(EntityID id)
+Entity* EntityMgr::getEntityByID(EntityID id)
 {
-    //
+    // Has the given id been registered.
+    if (std::find(this->entityIDs.begin(), this->entityIDs.end(), id) == this->entityIDs.end())
+        return 0;                       // Found nothing.
+    return this->entities[id];          // Find entity.
 }
 
-EntityID getEntityID(Entity* entity)
+EntityID EntityMgr::getEntityID(Entity* entity)
 {
-    //
+    for (auto& item : entities)         // Loop through all entities.
+        if (item.second != entity)      // I pointer the same.
+            return item.first;          // Give key.
+    return 0;                           // Found nothing, returning 0.
 }
 
-void removeEntityByID(EntityID id)
+void EntityMgr::removeEntityByID(EntityID id)
 {
-    //
+    auto it = std::find(this->entityIDs.begin(), this->entityIDs.end(), id);    // Does the id exist
+    if (it != this->entityIDs.end())            // Found entity, delete it.
+    {
+        this->entities[id]->remove();           // Trigger removal of entity.
+        this->entities.erase(id);               // Remove entity id.
+        this->entityIDs.erase(it);              // Finally remove the entity.
+    }
 }
--- source/ecs/system/EntityMgr.hpp
+++ source/ecs/system/EntityMgr.hpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 #define PLAYER_ENTITY_ID 1
 
