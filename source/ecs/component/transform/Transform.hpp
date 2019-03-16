fe501f8f6aee9357b7aea1f10542dc99bd52fa03
Xillez <kentwh@stud.ntnu.no>
2019-03-16 19:53:12 +0100

    Added unfinished transform class

---
+++ source/ecs/component/transform/Transform.hpp
@@ -0,0 +1,10 @@
+#include "../../entity/Entity.hpp"
+#include "../../component/Component.hpp"
+
+class Transform : public Component
+{
+private:
+    //Vector3 position;
+    //Quat rotation;
+    //Vector3 scale;
+}
