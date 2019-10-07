a7f82d2489d4fe78ce674a453cba4a6c6a20e507
Xillez <kentwh@stud.ntnu.no>
2019-10-07 20:58:28 +0200

    Config.hpp cmake changes

--- source/config/Config.hpp
+++ source/config/Config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// Add configuration variables in Config.hpp.in and not in header!
+// Add configuration variables in Config.hpp.in! Header will be overwritten by cmake!
 #define VERSION "v0.1"
 #define DEBUG true
 #define LOGGING true
