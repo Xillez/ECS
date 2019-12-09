59c04df7d5dbff29c9582baaf78bd29e22624f0e
Xillez <kentwh@stud.ntnu.no>
2019-12-09 21:33:15 +0100

    Cleaned up CMakeLists.txt and removed apple in build and fixed linux build

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,28 +1,15 @@
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  message("Linux build")
+if(UNIX)
   SET(LINUX 1)
-  SET(APPLE 0)
-  SET(WINDOWS 0)
-elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  message("Mac build")
-  SET(LINUX 0)
-  SET(APPLE 1)
   SET(WINDOWS 0)
 elseif(WIN32)
-  message("Windows build")
-  SET(WINDOWS 1)
   SET(LINUX 0)
-  SET(APPLE 0)
+  SET(WINDOWS 1)
 endif()
 
 # OS specific before executable addition
 if(WINDOWS)
   set(CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
 endif(WINDOWS)
-
-if(APPLE)
-  #
-endif(APPLE)
 
 if(LINUX)
   set(CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
