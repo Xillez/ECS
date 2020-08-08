ccd353372e03e581efa05bd6f1519681ee87abd8
Xillez <kent.wincent46@gmail.com>
2020-08-08 13:10:53 +0200

    Added definition for functions in Lists.hpp and updated documentation

--- source/ecs/system/lists/List.hpp
+++ source/ecs/system/lists/List.hpp
@@ -1,10 +1,12 @@
 #pragma once
-#include "typedef.hpp"
+#include "../typedef.hpp"
 
 #include <vector>
 #include <unordered_map>
+#include <functional>
 #include <algorithm>
 #include <type_traits>
+#include <typeinfo>
 
 // ################################################################################################
 // #                                                                                              #
@@ -20,10 +22,10 @@ namespace ECS
     /**
      * @brief List class. Contains a set of elements.
      * 
-     * @tparam Ttype Class/Type you wanna store.
+     * @tparam Ttype Class/Type you want to store.
      */
     template<class Ttype>
-    class List : private std::vector<Ttype>
+    class List
     {
     public:
         /**
@@ -51,38 +53,55 @@ namespace ECS
 
         /**
          * @brief Get the item by index.
+         * @attention NOT IMPLEMENTED!
          * 
          * @param index - Index of given item.
          * 
          * @return Ttype* - Pointer to item found. If nothing found, nullptr returned.
          */
-        //Ttype* GetItemByIndex(int index);
+        Ttype* GetItemByIndex(int index)
+        {
+            return nullptr;
+        }
 
         /**
          * @brief Get the index and item is stored.
+         * @attention NOT IMPLEMENTED!
          * 
          * @param item - Item to get index of.
          * 
          * @return int - Index of item. Negative if not found.
          */
-        //int GetItemIndex(Ttype* item);
+        int GetItemIndex(Ttype* item)
+        {
+            return 0;
+        }
 
         /**
          * @brief Get the item by type
+         * @attention NOT IMPLEMENTED!
          * 
          * @tparam Tclass - Class/Type to check for.
          * 
          * @return A std::vector of specified type if found.
          */
-        //template<class Tclass>    
-        //std::vector<Ttype> GetItemByType();
+        template<class Tclass>
+        std::vector<Ttype*> GetItemByType()
+        {
+            return std::vector<Ttype*>();
+        }
 
         /**
          * @brief Removes an item by index.
+         * @attention NOT IMPLEMENTED!
          * 
          * @param index Index to be removed.
+         *
          */
-        //void RemoveByIndex(int index);
+        void RemoveByIndex(int index)
+        {
+            //
+        }
 
         /**
          * @brief Removes item. If nullptr is given, entire list is deleted.
@@ -109,18 +128,27 @@ namespace ECS
          * 
          * @param func - Function to call for each item. Function signature: void(*func)(int, Ttype*).
          */
-        void ForEachItem(std::function<void(int, Ttype*)> func);
+        void ForEachItem(std::function<void(int, Ttype*)> func)
+        {
+            for (int i = 0; i < this->data.size(); i++)
+            {
+                func(i, this->data[i]);
+            }
+        }
 
         /**
          * @brief Returns the list as std::vector.
          * 
-         * @return An std::vector of all items.
+         * @return An std::vector containing all items.
          */
-        std::vector<Ttype> RawVector();
+        std::vector<Ttype*> RawVector()
+        {
+            return this->data;
+        }
 
     protected:
         //
     private:
-        std::vector<Ttype> list;      //!< A vector of all registered items.
+        std::vector<Ttype*> list;      //!< A vector of all registered items.
     };
-}    
+}
